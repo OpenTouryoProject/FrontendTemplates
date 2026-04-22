42f25abc75c9429af87007e3e86a745a4aa9f2b3
nishi_74322014 <nishi_74322014@ksj.biglobe.ne.jp>
2026-04-22 14:52:26 +0900

    Reboot:13

--- UI/XPlat/flutter_template/lib/main.dart
+++ UI/XPlat/flutter_template/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My App',
       theme: ThemeData(
-                // This is the theme of your application.
+        // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a purple toolbar. Then, without quitting the app,
